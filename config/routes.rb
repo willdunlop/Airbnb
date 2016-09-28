4653608fefa51619b94404b55390784d9f31d631
willdunlop <william.dunlop.1993@gmail.com>
2016-09-28 15:18:04 +1000

    end of day

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   resources :rooms
   devise_for :users
-  get 'pages/home'
+  root 'rooms#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'rooms#index'
 
 end
